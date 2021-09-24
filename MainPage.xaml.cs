54c862217526ac2bf6f574588dd65d83ba400d9b
Point-SimoSiren <45048404+Point-SimoSiren@users.noreply.github.com>
2021-09-24 14:58:24 +0300

    Muu projektikoodi

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CSSstylesXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void nappi_Clicked(object sender, EventArgs e)
+        {
+            if (teksti.Text != "Virta päällä!")
+            {
+                nappi.Background = Brush.Blue;
+                teksti.Text = "Virta päällä!";
+                teksti.TextColor = Color.Red;
+            }
+            else
+            {
+                nappi.Background = Brush.Red;
+                teksti.Text = "Virta pois päältä!";
+                teksti.TextColor = Color.Green;
+            }
+        }
+    }
+}
